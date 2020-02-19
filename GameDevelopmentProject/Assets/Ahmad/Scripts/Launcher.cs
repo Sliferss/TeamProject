165726803bbcb4bc955eaf459fb27fab6d7be9c3
Sliferss <60744269+Sliferss@users.noreply.github.com>
2020-02-19 19:49:51 +0000

    Minor change

---
+++ GameDevelopmentProject/Assets/Ahmad/Scripts/Launcher.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launcher : MonoBehaviour
+{
+    public GameObject rocket;
+    public float speed = 500f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            GameObject instRocket = Instantiate(rocket, transform.position, Quaternion.identity) as GameObject;
+            Rigidbody instRocketRigidbody = instRocket.GetComponent<Rigidbody>();
+            instRocketRigidbody.AddForce(Vector3.forward * speed);
+        }
+    }
+}
